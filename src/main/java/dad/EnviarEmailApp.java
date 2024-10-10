55108192975a336874478a1b8ecc00007fd26c1e
cristianQY <127297206+cristianQY@users.noreply.github.com>
2024-10-10 13:44:10 +0100

    Initial commit

---
+++ src/main/java/dad/EnviarEmailApp.java
@@ -0,0 +1,12 @@
+package dad;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class EnviarEmailApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
